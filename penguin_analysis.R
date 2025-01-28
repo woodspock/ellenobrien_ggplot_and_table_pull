ca042fba0e5dec01567408c5c2304244929042ea
woodspock <woodspock@gmail.com>
2025-01-28 10:18:24 +0100

    transfert RS vers GH

---
+++ penguin_analysis.R
@@ -0,0 +1,34 @@
+library(palmerpenguins)
+library(ggplot2)
+library(dplyr)
+library(kableExtra)
+
+data(package = "palmerpenguins")
+
+# Look at distribution of bill lengths in each species
+
+palette = c("#999999", "#e69f00", "#56b4e9", "#643a71")
+theme_set(
+  theme_classic(base_size = 7)
+)
+
+px <- ggplot(data = penguins, aes(bill_length_mm)) +
+  geom_histogram(aes(fill = species), alpha = 0.6, position = "identity") +
+  theme_classic() +
+  theme(text = element_text(size = 5),
+        legend.key.size = unit(.3, "cm")) +
+  xlab("Bill Length (mm)") +
+  ylab("Count") +
+  ggtitle("Penguin Noses") +
+  scale_fill_manual("Species", values = palette)
+
+px
+
+ggsave("my_penguin_plot.png", px, device = "png", width = 3, height = 1.5, dpi = 300)
+
+# Report average measures of beak/body per species
+t <- penguins %>% 
+  select(c("species", "bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g" )) %>%
+  aggregate(. ~ species, ., FUN = mean, na.rm = TRUE) %>%
+  kable() %>% 
+  cat(., files = "mymodel.html")
